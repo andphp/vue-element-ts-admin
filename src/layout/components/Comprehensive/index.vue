e8eb938cb550b79a5706fa291950809267aa48a5
BabySeeME「大雄」 <417170808@qq.com>
2021-10-11 15:47:06 +0800

    add vertical -layout

--- src/layout/components/Comprehensive/index.vue
+++ src/layout/components/Comprehensive/index.vue
@@ -1,11 +1,22 @@
 <template>
   this is layout of comprehensive.
+  <router-view />
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-}
+export default defineComponent({
+  name: 'Comprehensive',
+  props: {
+    device: {
+      type: String,
+      default() {
+        return "desktop";
+      },
+    },
+  }
+})
 </script>
 
 <style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,39 +1,26 @@
 <template>
-  <div :class="classObj" class="apa-wrapper">
-    <component
+     <component
       :is="layout"
-      :device="device"
     ></component>
-    {{layout}}
-  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed } from 'vue'
 import { useStore } from '@/store/index'
 import vertical from './components/Vertical/index.vue'
+import comprehensive from './components/Comprehensive/index.vue'
 
 export default defineComponent({
   name: 'Layout',
   components: {
-    vertical
+    vertical,
+    comprehensive
   },
   setup(){
     const store = useStore()
-    const device = computed(() => store.getters['appModule/getDeviceState'])
     const layout = computed(() => store.getters['settingsModule/getLayoutStyle'])
-    const opened = computed(() => store.getters['appModule/getSidebarState'])
-    const withoutAnimation = computed(() => store.getters['appModule/getSidebarAnimation'])
-    const classObj = computed(() => ({
-      hideSidebar: !opened.value,
-      openSidebar: opened.value,
-      withoutAnimation: withoutAnimation.value,
-      mobile: device.value === 'mobile'
-    }))
     return {
-      device,
       layout,
-      classObj
     }
   }
 })
